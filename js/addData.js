3c6d17b5617123436e34f05ae55cb552992ab308
Isaiah <isaiahyonas@gmail.com>
2018-11-15 16:25:41 -0800

    Fixed JS and database

--- js/addData.js
+++ js/addData.js
@@ -1,12 +1,10 @@
-const form = document.querySelector('#add-user-input');
-
-form.addEventListener('submit', function(evt) {
-	evt.preventDefault();
-	db.collection('userInput').add({
-		name: form.name.value,
-		email: form.email.value,
-		message: form.message.value
-});
+const form = document.querySelector('#add-user-input'); form.addEventListener('submit', function(evt) {
+evt.preventDefault();
+db.collection('userInput').add({
+	name: form.name.value,
+	email: form.email.value,
+	message: form.message.value
+	});
 form.name.value = '';
 form.email.value = '';
 form.message.value = '';
