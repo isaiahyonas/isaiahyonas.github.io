b991e045864e79ca84799fa3710bbfb15ddd8031
Isaiah <isaiahyonas@gmail.com>
2018-11-16 10:40:07 -0800

    entered phone number into form

--- js/addData.js
+++ js/addData.js
@@ -2,11 +2,15 @@ const form = document.querySelector('#add-user-input');
 form.addEventListener('submit', function(evt) {
 	evt.preventDefault();
 	db.collection('userInput').add({
+		db.collection('userInput').add({
+createdAt: firebase.firestore.FieldValue.serverTimestamp(),
 		name: form.name.value,
 		email: form.email.value,
+		phone: form.phone.value,
 		message: form.message.value
 	});
 	form.name.value = '';
 	form.email.value = '';
+	form.phone.value = '';
 	form.message.value = '';
 })
